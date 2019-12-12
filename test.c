250e99ab8cd0098e630c7534084903e9ab515e3c
Anoop S (anos2) <anos2@cisco.com>
2019-12-12 21:59:53 +0530

    one

--- test.c
+++ test.c
@@ -2,20 +2,45 @@
 #include "conio.h"
 
 int myadd(int a, int b);
+void push();
+void pop();
 
-int main() 
+void main() 
 {
-    int res;
+    int choice;
     
-    myadd(10, 30);
-    
-    res = myadd(40, 50);
-    
-    printf("%d\n", res);
+    do {
+        clrscr();
+        printf("\n----Main Menu\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Exit\n");
+        printf("Enter yout choice\n");
+        scanf("%d",&choice);
+        switch (choice) {
+            case 1:
+                printf("Choice 1\n");
+                push();
+                break;
+            case 2:
+                printf("Choice 2\n");
+                pop();
+                break;
+            case 3:
+                printf("Choice 3\n");
+                exit(0);
+                break;
+        }
+        
+    } while(1);
     
     getch();
 }
 
-int myadd(int a, int b) {
-    return a + b;
+void push() {
+    printf("Push\n");
+}
+
+void pop() {
+    printf("Pop\n");
 }
