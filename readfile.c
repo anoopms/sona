bfab6c01a9b09ed3642a164fc35bf4bd921a9923
Anoop MS <msmailbox@gmail.com>
2020-01-06 14:20:44 +0530

    updated

--- readfile.c
+++ readfile.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-int main()
-{
+
+int main()
+{
     char buff[1000];
-    char *ret;	FILE *fp;
- 	 fp = fopen("/Users/anos2/learn/sona/sample.txt", "r");
+    char *ret;
+	FILE *fp;
+    
+    fp = fopen("/Users/anos2/learn/sona/sample.txt", "r");
 
     if(fp == NULL)
     {
         printf("Cant open file");
         return 0;
     }
+
     while(fgets(buff, 1000, fp) != NULL)
     {
         printf(buff);
